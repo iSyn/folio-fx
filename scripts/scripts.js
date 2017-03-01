7607f712af3cfabdc37e77cfe676bf8242c67cb2
Synclair Wang <synclairwang11@yahoo.com>
2017-03-01 02:20:26 -0500

    did a bunch of shit, changed to card layout

---
+++ scripts/scripts.js
@@ -0,0 +1,21 @@
+$(() => {
+  console.log('scripts.js connected')
+
+  $(".typed-example").typed({
+      strings: [
+        "Wow, what a useful website",
+        "The creator of this site must be super cool",
+        "This plugin looks really neat!",
+        "It's very customizable too.",
+        "You can pause ^500 . ^500 . ^500 .",
+        "It can aso fix er",
+        "It can also fix errors!"
+        ],
+      typeSpeed: 0,
+      loop: true
+  });
+
+
+
+
+})
